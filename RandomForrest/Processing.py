8b55f5a496c8549f71debcdecdaac06fad40bb6d
rc2000123 <toametru2000@gmail.com>
2021-04-16 12:44:30 +0800

    Delete RandomForrest directory

--- RandomForrest/Processing.py
+++
@@ -1,6 +0,0 @@
-# Pandas is used for data manipulation
-import pandas as pd
-# Read in data and display first 5 rows
-features = pd.read_csv('dem.csv',sep=',')
-#features.head(5)
-#print (features.to_string())
