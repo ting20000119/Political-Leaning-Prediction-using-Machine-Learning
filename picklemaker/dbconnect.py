5bd8613b16768387bbfaaaab4dac9831f567f329
rc2000123 <toametru2000@gmail.com>
2021-05-24 16:44:43 +0800

    Add files via upload
    Picklemaker used to create pkl file of finallist

---
+++ picklemaker/dbconnect.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python
+import psycopg2
+from config import config
+import datetime
+conn = None
+def connect():
+    """ Connect to the PostgreSQL database server """
+    params = config()
+    print('Connecting to the PostgreSQL database...')
+    global conn
+    conn = psycopg2.connect(**params)
+
+def disconnect():
+    global conn
+    conn.close()
+    print('Database connection closed.')
+
+def getsubscribercount():
+    cur = conn.cursor()
+    insert_stmt = (
+        "SELECT * from subreddit_infonew"
+        )
+    cur.execute(insert_stmt)
+    result = cur.fetchall()
+    return dict(result)
+    cur.close()
+
+def getdeletedsubreddits():
+    cur = conn.cursor()
+    insert_stmt = (
+        "SELECT subreddit from subreddit_infonew where subscribers = 0"
+        )
+    cur.execute(insert_stmt)
+    result = cur.fetchall()
+    cur.close()
+    result = [x for (x,) in result]
+#    print(result)
+    return result
+
+def getdeminfo():
+    cur = conn.cursor()
+    insert_stmt = (
+        "SELECT author,subreddit,score from demactivity"
+        )
+    cur.execute(insert_stmt)
+    result = cur.fetchall()
+    cur.close()
+    return result
+
+def getrepinfo():
+    cur = conn.cursor()
+    insert_stmt = (
+        "SELECT author,subreddit,score from repactivity"
+        )
+    cur.execute(insert_stmt)
+    result = cur.fetchall()
+    cur.close()
+    return result
+
+def executeSQL(insert_stmt):
+    cur = conn.cursor()
+    cur.execute(insert_stmt)
+    cur.close()
+
+def main():
+    connect()
+    insertdb('key5','testtitle', 'http://testurl', 'testauthor', 10, datetime.date(2012, 3, 23), '123456',11,'testperm','testflair')
+    disconnect()
+if __name__ == '__main__':
+    main()
